2757e60368707ea258b8472f2c3991f23f639c61
John Doe <nickgray0@gmail.com>
2022-07-15 19:42:10 -0400

    cosmetics

--- src.cxx
+++ src.cxx
@@ -5,41 +5,41 @@
 #include <tuple>
 #include <concepts>
 
-// task 2, task 3: TimesTwo() here
+// task 2, task 3: timesTwo() here
 
 struct Rectangle {
-    double Length; // data member, also known as a field
-    double Width = 1; // field can have a default value
+    double length; // data member, also known as a field
+    double width = 1; // field can have a default value
     // fields must be explicitly typed, you cannot use type deduction here
 
     // member function, also known as a method
-    double CalculateArea() {
-        return Length * Width;
+    double calculateArea() {
+        return length * width;
     }
 
     // member function that modifies the state of an object
-    void MakeItSquare(double SideLength) {
-        Length = SideLength;
-        Width = SideLength;
+    void makeItSquare(double sideLength) {
+        length = sideLength;
+        width = sideLength;
     }
 
-    // task 4: CalculatePerimeter() here
+    // task 4: calculatePerimeter() here
 };
 
 // task 5: struct Circle here
 
 // task 6:
-void PrintShape(auto Shape) {
+void printShape(auto shape) {
     // Your code here
 }
 
 // task 9:
-void DoubleEachElement(/* ??? Container */) {
+void doubleEachElement(/* ??? container */) {
     // your code here
 }
 
 // task 10, task 11:
-void PrintEachObject(auto* PointerToTheFirstObject, std::size_t NumberOfObjects) {
+void printEachObject(auto* pointerToTheFirstObject, std::size_t numberOfObjects) {
     // your code here
 }
 
@@ -47,19 +47,19 @@ int main() {
     // task1: write "Hello World!" in an std::cout << "" << std::endl; call
 
     // task2
-    // std::cout << TimesTwo(21) << std::endl;
+    // std::cout << timesTwo(21) << std::endl;
 
     // task3
-    // std::cout << TimesTwo(123) << std::endl;
-    // std::cout << TimesTwo(3.14) << std::endl;
-    // std::cout << TimesTwo(std::string{ "abc" }) << std::endl;
+    // std::cout << timesTwo(123) << std::endl;
+    // std::cout << timesTwo(3.14) << std::endl;
+    // std::cout << timesTwo(std::string{ "abc" }) << std::endl;
 
     // task4
-    // std::cout << Rectangle{ 7, 8 }.CalculatePerimeter() << std::endl;
+    // std::cout << Rectangle{ 7, 8 }.calculatePerimeter() << std::endl;
 
     // task5: create a few instances of Circle, and call their member functions
 
-    // task6: call PrintShape with different Rectangle and Circle objects
+    // task6: call printShape with different Rectangle and Circle objects
 
     // task7: create a container of strings, fill the container with some strings
     //        repeat each string element in the container, print each string element in the container
@@ -71,12 +71,12 @@ int main() {
     // static_assert(std::same_as<decltype(*ppx), /* type of *ppx here */>);
     // static_assert(std::same_as<decltype(**ppx), /* type of **ppx here */>);
 
-    // task9: pass different std::array and std::vector objects to DoubleEachElement
-    //        print the results after DoubleEachElement calls
+    // task9: pass different std::array and std::vector objects to doubleEachElement
+    //        print the results after doubleEachElement calls
 
     // task10:
     // auto v1 = std::vector{ "aaa", "bbb", "ccc", "hello" };
     // auto v2 = std::array{ 7, 8, 9, 10, 11 };
-    // PrintEachObject(v1.data(), v1.size());
-    // PrintEachObject(v2.data(), v2.size());
+    // printEachObject(v1.data(), v1.size());
+    // printEachObject(v2.data(), v2.size());
 }
